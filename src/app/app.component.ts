73b1edec39674d801917af15e779c24b62ab518b
dpMelian <astrodaniel1234@gmail.com>
2020-04-08 21:20:45 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
+/**
+ * @title Simple autocomplete
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ShoppingList';
+// tslint:disable-next-line:component-class-suffix
+export class AutocompleteSimpleExample implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['Ketchup', 'Pan', 'Queso', 'Tomate', 'Aceite de oliva'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    console.log(value);
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
