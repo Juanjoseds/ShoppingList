e953cdda52bab71c06beedfbb14f3b5d415138e3
Juan José <diazsantanajuanjose@gmail.com>
2020-04-08 20:45:07 +0100

    Implementación de añadir producto al modelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,49 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'ShoppingList';
+  model = {
+    user: 'DAW',
+    items: [],
+    cart: []
+  };
+
+  addItem(producto) {
+    if (this.model.items.length !== 0) {
+      // Comprobamos si ya existe el producto
+      let duplicado = false;
+      this.model.items.forEach((item) => {
+        if (item.product === producto) {
+          duplicado = true;
+        }
+      });
+
+      // Si el producto existe lo añadimos a la lista de la compra
+      if (duplicado) {
+        this.addShoppingList(producto);
+      } else {
+        // Si no existe, lo añadimos al JSON y a la lista de la compra
+        this.model.items.push({product: producto, bought: false, supermarket: null, price: null});
+        this.addShoppingList(producto);
+      }
+    // Si el JSON está vacío lo añadimos al JSON y a la lista de la compra
+    } else {
+      this.model.items.push({product: producto, bought: false, supermarket: null, price: null});
+      this.addShoppingList(producto);
+    }
+    console.log(this.model.cart);
+    console.log(this.model.items);
+  }
+
+  getProducts() {
+    let products: string[] = [];
+    this.model.items.forEach((item, index) => products.push(item.product));
+    return products;
+
+  }
+
+  addShoppingList(producto){
+    if (this.model.cart.indexOf(producto) === -1){
+      this.model.cart.push(producto);
+    }
+  }
 }
--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,14 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {AppComponent} from './app.component';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
-import {startWith, map, debounceTime} from 'rxjs/operators';
+import {startWith, map, debounceTime, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'search-bar',
   template: `
       <div class="card-body">
         <div class="input-group input-group-sm mb-2">
-          <input class="form-control form-margin" type="text"
+          <input class="form-control form-margin" type="text" #item
                  placeholder="Buscar producto..."
                  aria-label="Number"
                  [formControl]="myControl"
@@ -19,32 +20,32 @@ import {startWith, map, debounceTime} from 'rxjs/operators';
             </mat-option>
           </mat-autocomplete>
           <div class="input-group-append">
-            <button class="btn btn-outline-secondary">Añadir</button>
+            <button class="btn btn-outline-secondary" (click)="appComponent.addItem(myControl.value); myControl.setValue('');">Añadir</button>
           </div>
         </div>
 `
 })
 export class SearchComponent implements OnInit{
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  options: string[] = this.appComponent.getProducts();
   filteredOptions: Observable<string[]>;
 
-  constructor() {}
+  constructor(public appComponent: AppComponent) {}
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
         // Tiempo de retardo para cada petición
-        debounceTime(400),
+        debounceTime(500),
         map(value => this._filter(value))
       );
   }
 
   private _filter(value: string): string[] {
-    console.log(value);
     const filterValue = value.toLowerCase();
-
+    this.options = this.appComponent.getProducts();
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
+
 }
